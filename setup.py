9f1760bb8d8dedc37d48e44ced0cc108ccd1d7e2
NaLv2004 <213221582@seu.edu.cn>
2024-09-30 00:35:44 +0800

    delete original files: 240929

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='voldelog',
-    version='0.1.0',
-    description='Stay Evil',
-    packages=find_packages(),
-)
--- voldelog/__init__.py
+++
@@ -1,3 +0,0 @@
-from .voodoo import voodoo
-
-__all__ = ['voodoo']
