c15fcc7b8a64710ff13bdd1aab941c5e69c8dc24
NaLv2004 <146506744+NaLv2004@users.noreply.github.com>
2024-09-30 01:45:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pytv',
+    name='PyTV',
     version='0.1',
     packages=find_packages(),
-    description='Simple math operations',
+    description='RTL Auto-generation with Verilog Embedded in Python',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='JiaYan Xu',
-    author_email='your.email@example.com',
+    author_email='jiayanxu@.com',
     url='https://github.com/yourusername/simplemath',
     license='MIT',
     python_requires='>=3.6',
