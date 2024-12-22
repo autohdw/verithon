aada301a983a1484ae9a24cd8a3e6bb5baa31b38
NaLv2004 <213221582@seu.edu.cn>
2024-12-22 14:09:09 +0800

    verithon 0.5: support decorating class methods

--- demo/mycls.py
+++
@@ -1,6 +0,0 @@
-class QuType:
-    DWT = 8
-    FRAC = 4
-    def __init__(self,DWT,FRAC):
-        self.DWT = DWT
-        self.FRAC = FRAC
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='verithon',
-    version='0.3',
+    version='0.5',
     packages=find_packages(),
     description='RTL Auto-generation with Verilog Embedded in Python',
     long_description=open('README.md').read(),
